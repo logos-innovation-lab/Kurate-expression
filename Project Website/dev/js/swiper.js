46f13e58aa9a99f9832780e9076bfa327b944297
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-03-30 14:32:02 -0400

    feat: move coverflow when visible

--- Project Website/dev/js/swiper.js
+++ Project Website/dev/js/swiper.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-	// initialSlide: 6,
+	initialSlide: 0,
 	loop: true,
 	effect: "coverflow",
 	grabCursor: true,
@@ -17,3 +17,29 @@ var swiper = new Swiper(".mySwiper", {
 		el: ".swiper-pagination",
 	},
 });
+
+let first = true;
+
+const mySwiper = document.querySelector('.mySwiper');
+
+function isInViewport(elem) {
+	var bounding = elem.getBoundingClientRect();
+	return (
+		bounding.top >= 0 &&
+		bounding.left >= 0 &&
+		bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+
+document.addEventListener('scroll', function () {
+	if (isInViewport(mySwiper)) {
+		if (first) {
+			swiper.slideNext(500, true);
+			first = false;
+		}
+	} else {
+		first = true;
+	}
+})
